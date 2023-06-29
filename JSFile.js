7315931e214e3c91b4ddf62d3b120735a66d8029
Nurasik12 <nurasylserikov4@gmail.com>
2023-06-29 11:30:10 +0600

    Start

---
+++ JSFile.js
@@ -0,0 +1,22 @@
+
+const like = document.getElementById("AddLike");
+
+like.addEventListener(
+    "click", 
+    () => {
+    
+    alert("Click");
+  },
+  true
+  );
+
+  const btn = document.getElementById("button");
+
+  btn.addEventListener(
+    "dblclick",
+    a,
+  );
+
+  function a(){
+    alert("double-click")
+  }
