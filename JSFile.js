71fc6b9da4d9b15230536440c9f1bf5d0d31aa59
Nurasik12 <nurasylserikov4@gmail.com>
2023-07-28 11:36:55 +0600

    New commit

--- JSFile.js
+++ JSFile.js
@@ -1,22 +1,53 @@
+// const like = document.getElementById("AddLike");
 
-const like = document.getElementById("AddLike");
+// like.addEventListener(
+//     "click",
+//     () => {
 
-like.addEventListener(
-    "click", 
-    () => {
-    
-    alert("Click");
-  },
-  true
-  );
+//     alert("Click");
+//   },
+//   true
+//   );
 
-  const btn = document.getElementById("button");
+//   const btn = document.getElementById("button");
 
-  btn.addEventListener(
-    "dblclick",
-    a,
-  );
+//   btn.addEventListener(
+//     "dblclick",
+//     a,
+//   );
 
-  function a(){
-    alert("double-click")
+//   function a(){
+//     alert("double-click")
+//   }
+
+// const input1 = document.getElementById("input1");
+
+// input1.addEventListener("change", () => {
+//   document.getElementsByClassName("Yes").innerHTML = "Hi1";
+// });
+let k = 0;
+
+function change() {
+  if (k % 2 == 0) {
+    document.querySelector("header").innerHTML = "<h1>Changed</h1>";
+  } else {
+    document.querySelector("header").innerHTML =
+      "<h1>To-Dos</h1> <p>(returned)</p>";
   }
+  k++;
+}
+
+const el = document.getElementsByTagName("li");
+function Display_length() {
+  const p = document.querySelector("p");
+  // el[1].innerHTML = `<h1>${el[0].innerHTML}</h1>`;
+  p.innerHTML = `Length   --->    ${el.length}`;
+}
+
+function AddUl() {
+  if (el.length < 14) {
+    const ul = document.querySelector("ul");
+    ul.innerHTML += "<li></li>"
+  }
+  else { alert("The limit of ul's has been exceeded"); }
+}
---
+++ js-practice.js
@@ -0,0 +1,25 @@
+// Code an arrow function that adds 10% to each value in the prices array.
+
+// const prices = [1.99, 2.49, 0.99, 4.99];
+
+//                        :1:Method
+
+// const taxed = prices.reduce((prev, curr, index) => {
+//   if (index == 1) {
+//     return prev * 0.1 + curr * 0.1;
+//   }
+//   return prev + curr * 0.1;
+// });
+
+// console.log(taxed);
+
+//                    :2:Method
+
+// const prices = [1.99, 2.49, 0.99, 4.99];
+
+// const taxed = prices.map((element) => {
+//   return element * 0.1;
+// });
+
+// console.log(taxed.reduce((prev, curr) => prev + curr));
+
